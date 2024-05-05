93def0a85d62a2d8bd38274167636fe209e56c0c
kimnamju <kimnamju0920@gmail.com>
2024-05-05 18:15:43 +0900

    fix: Google Cloud 연결을 정적으로 변경

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ class BERTClassifier(nn.Module):
         if self.dr_rate:
             out = self.dropout(pooler)
         return self.classifier(out)
-    
 
 import torch
 from kobert_tokenizer import KoBERTTokenizer
@@ -52,8 +51,6 @@ transform = nlp.data.BERTSentenceTransform(
             tok, max_seq_length=max_len, vocab=vocab, pad=True, pair=False)
 
 # 모델 불러오기
-# model = BERTClassifier(bertmodel,  dr_rate=0.4).to(device)
-# model.load_state_dict(torch.load('/content/drive/MyDrive/dl-team-project/model_state_dict.pt',map_location=torch.device('cpu')))
 model = BERTClassifier(bertmodel,  dr_rate=0.4).to(device)
 model.load_state_dict(torch.load('./model_state_dict.pt',map_location=torch.device('cpu')))
 model.eval()
@@ -77,85 +74,22 @@ def predict_vp(input_text:str):
 
 # Model Serving
 # --------------------------------------------------------------------------------------------
-# Server
-
-import shutil
-from typing import Optional, List
-from fastapi import FastAPI, File, UploadFile
-from pydantic import BaseModel
-import base64
-import os
-
-
-
-app = FastAPI()
-
-class AudioData(BaseModel):
-    data: str 
-
+# Google Cloud Services
 
 # cloud speech-to-text v2
 from google.cloud.speech_v2 import SpeechClient
 from google.cloud.speech_v2.types import cloud_speech
 from google.cloud import storage
 
-def get_transcribe(audio_data:str)->str:
-    # Instantiates a client
-    client = SpeechClient()
-
-    config = cloud_speech.RecognitionConfig(
-        auto_decoding_config=cloud_speech.AutoDetectDecodingConfig(),
-        language_codes=["ko-KR"],
-        model="long",
-    )
-
-    request = cloud_speech.RecognizeRequest(
-    recognizer=f"projects/solar-attic-406607/locations/global/recognizers/_",
-    config=config,
-    content=audio_data,
-    )
-
-    # Detects speech in the audio file
-    response = client.recognize(request=request)
-
-    result_transcribe = ""
-    for result in response.results:
-        result_transcribe += result.alternatives[0].transcript
-        result_transcribe += "\n"
-    
-    return result_transcribe
-
-
-BUCKET_NAME = "stt-audio-dl"
-PROJECT_ID = 'solar-attic-406607'
-DESTINATION_BLOB_NAME = 'vp-detect-audio'
-
-def get_blob_name_with_timestamp()->str:
-    timestamp = datetime.timestamp(datetime.now())
-    formatted_time = datetime.fromtimestamp(timestamp).strftime('-%y%m%d-%H:%M:%S')
-    return DESTINATION_BLOB_NAME + formatted_time + '.raw'
-
-def get_gcs_uri_from_blob_name(blob_name: str)->str:
-    return 'gs://' + BUCKET_NAME + '/' + blob_name
-
-def upload_to_gcs(contents, blob_name:str):
-    """Uploads a audio file to the bucket."""
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(BUCKET_NAME)
-    blob = bucket.blob(blob_name)
-
-    blob.upload_from_string(contents)
-
+# cloud speech-to-text v2 config setting
+client = SpeechClient()
+config = cloud_speech.RecognitionConfig(
+    auto_decoding_config=cloud_speech.AutoDetectDecodingConfig(),
+    language_codes=["ko-KR"],
+    model="long",
+)
 # 1분 이상의 오디오 파일의 stt 가능
 def get_transcribe_v2(gcs_uri:str)->str:
-    client = SpeechClient()
-
-    config = cloud_speech.RecognitionConfig(
-        auto_decoding_config=cloud_speech.AutoDetectDecodingConfig(),
-        language_codes=["ko-KR"],
-        model="long",
-    )
-
     file_metadata = cloud_speech.BatchRecognizeFileMetadata(uri=gcs_uri)
 
     request = cloud_speech.BatchRecognizeRequest(
@@ -175,9 +109,39 @@ def get_transcribe_v2(gcs_uri:str)->str:
     for result in response.results[gcs_uri].transcript.results:
         result_transcribe += result.alternatives[0].transcript
         result_transcribe += '\n'
-    
     return result_transcribe
 
+BUCKET_NAME = "stt-audio-dl"
+PROJECT_ID = 'solar-attic-406607'
+DESTINATION_BLOB_NAME = 'vp-detect-audio'
+
+def get_blob_name_with_timestamp()->str:
+    timestamp = datetime.timestamp(datetime.now())
+    formatted_time = datetime.fromtimestamp(timestamp).strftime('-%y%m%d-%H:%M:%S')
+    return DESTINATION_BLOB_NAME + formatted_time + '.raw'
+
+def get_gcs_uri_from_blob_name(blob_name: str)->str:
+    return 'gs://' + BUCKET_NAME + '/' + blob_name
+
+storage_client = storage.Client()
+bucket = storage_client.bucket(BUCKET_NAME)
+def upload_to_gcs(contents, blob_name:str):
+    """Uploads a audio file to the bucket."""
+    blob = bucket.blob(blob_name)
+    blob.upload_from_string(contents)
+
+# Google Cloud Services
+# --------------------------------------------------------------------------------------------
+# API Server
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+import base64
+
+app = FastAPI()
+
+class AudioData(BaseModel):
+    data: str
 
 @app.post("/detect-phishing")
 async def detect_phishing(audio_data: AudioData):
@@ -186,14 +150,16 @@ async def detect_phishing(audio_data: AudioData):
     blob_name = get_blob_name_with_timestamp()
     gcs_uri = get_gcs_uri_from_blob_name(blob_name)
 
+    # 1. Upload audio data to GCS: GCS에 업로드해야 로컬 파일 시스템에 저장하지 않고 STT 사용 가능
     upload_to_gcs(decoded_data, blob_name)
+    # 2. Convert audio to text using Google STT
     transcript = get_transcribe_v2(gcs_uri)
+    # 3. Use custom KoBERT model to predict probability 
     res = predict_vp(transcript)
+    # 4. Return json response to client.
     return {"transcript":transcript, "result": str(res)}
-    
 
 
 import uvicorn
-    
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
